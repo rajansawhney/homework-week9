5ac3d121f709ea0f9bb0aaf31bc9943c62813ebd
rajansawhney <rajan.k.sawhney@gmail.com>
2023-10-23 14:13:44 -1000

    test updates

--- test.sh
+++ test.sh
@@ -4,7 +4,7 @@ echo
 echo "*****"
 echo
 echo "Find the largest number..."
-output_largest_num=$(./largest_num <test/input_largest_number)
+output_largest_num=$(./largest_number <test/input_largest_number)
 echo $output_largest_num
 expected_output_largest_num="24.20"
 
